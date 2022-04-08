5e7d4aee1adddeeae06879856d4aad3e83029c0c
Mizuki Hirose <hmizuki.scl@gmail.com>
2022-04-08 20:48:46 +0900

    automated tests file added.

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,16 +1,22 @@
 from pyexpat import model
 from django.db import models
+from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator
 
-from datetime import datetime
+import datetime
 
 # Create your models here.
 class Todo(models.Model):
   todo_text = models.CharField(max_length=200, validators=[MinLengthValidator(1)])
   is_done = models.BooleanField(default=False)
-  pub_date = models.DateTimeField('date published', default=datetime.now)
+  pub_date = models.DateTimeField('date published', default=datetime.datetime.now)
   due_date = models.DateField('due date', null=True)
   importance = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(2)])
 
+
   def __str__(self):
     return self.todo_text
+  
+
+  def was_published_recently(self):
+    return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=7)
--- mysite/todo/tests.py
+++ mysite/todo/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Todo
+
+import datetime
 
 # Create your tests here.
+
+class TodoModelTests(TestCase):
+  def test_was_published_recently_with_future_date(self):
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_published_todo = Todo(pub_date=time)
+    self.assertIs(future_published_todo.was_published_recently(), False)
+
+
+  def test_was_published_recently_with_old_date(self):
+    time = timezone.now() - datetime.timedelta(days=30)
+    old_published_todo = Todo(pub_date=time)
+    self.assertIs(old_published_todo.was_published_recently(), False)
+
+
+  def test_was_published_recently_with_recent_date(self):
+    time = timezone.now() - datetime.timedelta(days=3)
+    recently_published_todo = Todo(pub_date=time)
+    self.assertIs(recently_published_todo.was_published_recently(), True)
